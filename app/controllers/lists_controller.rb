becc646fae178d33ccbcbd6d7564913f2cc034e8
Sen Cao <caosen.cn@gmail.com>
2022-08-20 16:26:02 +0300

    done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,28 @@
 class ListsController < ApplicationController
+  before_action :find_params, only: [:show, :edit, :update, :destroy]
+  def edit
+    find_params
+  end
+
+  def update
+    find_params
+    @list.update(set_params)
+    redirect_to show-action-route-prefix-here_path(@var)
+  end
+
+  def destroy
+    find_params
+    @list.destroy
+    redirect_to index-action-route-prefix-here_path
+  end
+
+  private
+
+  def find_params
+    @list = List.find(params[:id])
+  end
+
+  def set_params
+    params.require(:list).permit(:name, :description)
+  end
 end
