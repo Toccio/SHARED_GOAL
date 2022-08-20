e7333f6e480034754fa2c07d950070cee88adf9b
Sen Cao <caosen.cn@gmail.com>
2022-08-20 13:44:38 +0300

    added validations to models

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ApplicationRecord
   has_many :skills, dependent: :destroy
   has_many :classrooms, dependent: :destroy
+  validates :name, presence: true
+  validates :description, presence: true
 end
