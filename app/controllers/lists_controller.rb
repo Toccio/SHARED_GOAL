756caf7b7750c5190e61af18496a135eb989bec6
luca visaggio <luca.t.visaggio@gmail.com>
2022-08-20 15:25:31 +0200

    new-create-show-index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,37 @@
 class ListsController < ApplicationController
+  before_action :find_params, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @list = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+    # authorize @list
+  end
+
+  def create
+    @list = List.new(set_params)
+#     @list.user = current_user
+    # authorize @list
+      if @list.save
+        redirect_to list_path(@list), notice: 'The list was successfully created!'
+      else
+        render :new
+      end
+  end
+
+  private
+
+  def find_params
+    @list = List.find(params[:id])
+  end
+
+  def set_params
+    params.require(:list).permit(:name, :description)
+  end
+
 end
