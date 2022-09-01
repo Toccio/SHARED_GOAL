6d78c5db58dce204f9a9c8aa5dee2908ff51c920
luca visaggio <luca.t.visaggio@gmail.com>
2022-09-01 19:44:15 +0200

    creating a chatroom new action

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,8 +1,23 @@
 class ChatroomsController < ApplicationController
+  def new
+    @chatroom = Chatroom.new
+  end
+
+  def create
+    @chatroom = Chatroom.new(set_chatroom)
+    authorize @chatroom
+    @chatroom.save
+  end
+
   def show
     @message = Message.new
     @chatroom = policy_scope(Chatroom).find(params[:id])
     authorize @chatroom
   end
 
+  private
+
+  def set_chatroom
+    params.require(:chatroom).permit(:name)
+  end
 end
