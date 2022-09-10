c9af904ea7f21d81985df995f245c6171665b9f5
luca visaggio <luca.t.visaggio@gmail.com>
2022-09-10 14:42:26 +0200

    seeds done

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -4,6 +4,7 @@ class ClassroomsController < ApplicationController
   def index
     @lists = policy_scope(List).all
     @classroom_category = policy_scope(ClassroomCategory).find(params[:classroom_category_id])
+    # raise
     # @list = @classroom_category.list
     if params[:query].present?
       @classrooms = Classroom.search_by_name_and_description(params[:query])
